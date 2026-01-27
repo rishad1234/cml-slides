8e7e8b779f2be186bf7ecd20fef3381c26b5e7da
rishad1234 <rishadulislam@outlook.com>
2026-01-27 09:34:14 +0600

    initial commit

---
+++ compute-paradigms/main.js
@@ -0,0 +1,10 @@
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
+import Notes from 'reveal.js/plugin/notes/notes.esm.js';
+
+Reveal.initialize({
+  hash: true,
+  slideNumber: true,
+  plugins: [Markdown, Highlight, Notes]
+});
